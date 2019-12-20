04c7e649630506786872a3a91befd5007dd6ca83
S. Makai Takori <makait87@gmail.com>
2019-12-20 05:41:32 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,8 +38,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   }
   
    holiday_hash.each do |season, holiday_name|
-    holiday_name.each do |attribute, value|
-     if attribute == :christmas && :new_years
+    season.each do |attribute, value|
+     if attribute == :christmas || :new_years
       value.each do |supply|
         puts "#{supply}"
       end  
