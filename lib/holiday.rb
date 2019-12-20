c90420f301db4f56aeb5607212ded0e999c2b7e0
S. Makai Takori <makait87@gmail.com>
2019-12-20 05:22:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
     }
   }
   
- holiday_hash[:winter][:christmas][:new_years] << "supply"
+ holiday_hash[:winter][:christmas] << "supply"
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
 
