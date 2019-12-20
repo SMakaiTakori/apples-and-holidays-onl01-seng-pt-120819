dead1f6817a06c7794edcf8bb3a58b5bef1d0500
S. Makai Takori <makait87@gmail.com>
2019-12-20 05:49:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,7 +38,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   }
   
    holiday_hash[:winter].each do |attribute, value|
-     value << 
+     value << supply
      
   end
   # holiday_hash[:winter][:christmas] << "Balloons"
