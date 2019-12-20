55e899e79a26a0dc58dde379f834d4159d730dcf
S. Makai Takori <makait87@gmail.com>
2019-12-20 05:29:09 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,10 +39,11 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   
    holiday_hash.each do |season, holiday_name|
      if season == :winter
-           
-   
-   holiday_hash[:winter][:christmas] << "Balloons"
-   holiday_hash[:winter][:new_years] << "Balloons"
+           puts "Balloons"
+     end       
+   end
+  # holiday_hash[:winter][:christmas] << "Balloons"
+  # holiday_hash[:winter][:new_years] << "Balloons"
    
   holiday_hash
 end
