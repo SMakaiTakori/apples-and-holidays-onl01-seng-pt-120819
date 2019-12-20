66f73a6d5fef2b5ebe2609eec2db887624d254ba
S. Makai Takori <makait87@gmail.com>
2019-12-20 05:58:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,8 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
     }
   }
   
-    holiday_hash[:season][:holiday_name]
+    holiday_hash[:season][:holiday_name] = supply_array
+    holiday_hash
   
   
   # remember to return the updated hash
