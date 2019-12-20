4f94a3343f59d11fba7b5483a73a25d0ac390f04
S. Makai Takori <makait87@gmail.com>
2019-12-20 06:17:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -127,7 +127,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holiday_name|
       puts "#{season.capitalize}:"
       holiday_name.each do |holiday, supply|
-        puts "#{holiday.to_s.split(" ").map {|i| i.capitalize}}.join("")}: #{supply.join(",")}"
+        puts "#{holiday.to_s.split("_").map {|i| i.capitalize}}.join("")}: #{supply.join(",")}"
       end
     end  
   
